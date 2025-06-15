0737ea03fe1d6225b6a071d61e7526acf576043b
JeffreyBarrios <jeffreybarrios629@gmail.com>
2025-06-14 18:11:24 -0600

    Se agrega opción de cotizador

--- IMSA_Frontend_App/src/app/app.routes.ts
+++ IMSA_Frontend_App/src/app/app.routes.ts
@@ -3,45 +3,50 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
 
     {
-        path :'dashboard',
+        path: 'dashboard',
         loadComponent: () => import('./home-dashboard/dashboard.component'),
         children: [
 
             {
-                path:'solicitudes-de-compra',
+                path: 'solicitudes-de-compra',
                 title: 'Solicitudes de compra',
-                loadChildren:() => import('./solicitudes-de-compra/ordenes-de-compra.route').then((m)=> m.ordenesCompraRoute)
+                loadChildren: () => import('./solicitudes-de-compra/ordenes-de-compra.route').then((m) => m.ordenesCompraRoute)
             },
-              {
-                path:'dashboard-de-solicitudes',
+            {
+                path: 'dashboard-de-solicitudes',
                 title: 'Dashboard de solicitudes',
-                loadChildren:() => import('./dashboard-solicitudes/dashboard-solicitudes.route').then((m)=> m.dashboardSolicitudesRoute)
+                loadChildren: () => import('./dashboard-solicitudes/dashboard-solicitudes.route').then((m) => m.dashboardSolicitudesRoute)
             },
-              {
-                path:'mantenimiento-clientes',
+             {
+                path: 'cotizador',
+                title: 'Cotizar',
+                loadChildren: () => import('./Cotizador/cotizador.route').then((m) => m.cotizadorRoute)
+            },
+            {
+                path: 'mantenimiento-clientes',
                 title: 'Mantenimiento de clientes',
-                loadChildren:() => import('./solicitudes-de-compra/ordenes-de-compra.route').then((m)=> m.ordenesCompraRoute)
+                loadChildren: () => import('./solicitudes-de-compra/ordenes-de-compra.route').then((m) => m.ordenesCompraRoute)
             },
             {
-                path:'mantenimiento-analistas',
+                path: 'mantenimiento-analistas',
                 title: 'Mantenimiento de analistas',
-                loadChildren:() => import('./solicitudes-de-compra/ordenes-de-compra.route').then((m)=> m.ordenesCompraRoute)
+                loadChildren: () => import('./solicitudes-de-compra/ordenes-de-compra.route').then((m) => m.ordenesCompraRoute)
             },
             {
-                path:'mantenimiento-proveedores',
+                path: 'mantenimiento-proveedores',
                 title: 'Mantenimiento de proveedores',
-                loadChildren:() => import('./solicitudes-de-compra/ordenes-de-compra.route').then((m)=> m.ordenesCompraRoute)
+                loadChildren: () => import('./solicitudes-de-compra/ordenes-de-compra.route').then((m) => m.ordenesCompraRoute)
             },
             {
-                path:'',
+                path: '',
                 redirectTo: "",
-                pathMatch:'full'
+                pathMatch: 'full'
             }
         ]
     },
     {
-        path:'',
+        path: '',
         redirectTo: "/dashboard",
-        pathMatch:'full'
+        pathMatch: 'full'
     }
 ];
---
+++ IMSA_Frontend_App/src/app/cotizador/cotizador.route.ts
@@ -0,0 +1,13 @@
+import { Routes } from "@angular/router";
+import { CotizadorComponent } from "./components/cotizador/cotizador.component";
+
+export const cotizadorRoute: Routes =[
+
+    {
+        path:'',
+        component: CotizadorComponent
+
+    },
+   
+    
+]
